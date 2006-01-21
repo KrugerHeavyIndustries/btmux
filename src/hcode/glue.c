f40ba2ac9e9f5dd419cae7480df5060221270737
dany <dany@e60b412a-5d77-420b-80c2-6cef64185626>
2006-01-21 04:58:39 +0000

    Brought the branch uptodate with trunk HEAD (r518)
    git-svn-id: https://svn.code.sf.net/p/btonline-btech/code/btmux/branches/btmux-hcode-xdr@520 e60b412a-5d77-420b-80c2-6cef64185626

--- src/hcode/glue.c
+++ src/hcode/glue.c
@@ -50,6 +50,7 @@
 #include "p.mechfile.h"
 #include "p.mech.stat.h"
 #include "p.mech.partnames.h"
+#include "debug.h"
 
 /* Prototypes */
 
@@ -488,7 +489,6 @@ static void load_xcode()
 	byte xcode_version;
 	int filemode;
 
-	RCache_Flush();
 	initialize_colorize();
 	fprintf(stderr, "LOADING: %s\n", mudconf.hcode_db);
 	f = my_open_file(mudconf.hcode_db, "r", &filemode);
