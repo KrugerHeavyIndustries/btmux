ba790e27cfff6d8d1505d9fd5f6278fe466f6e35
dany <dany@e60b412a-5d77-420b-80c2-6cef64185626>
2006-01-27 15:31:55 +0000

    Brought the branch up to match trunk's latest (r542)
    git-svn-id: https://svn.code.sf.net/p/btonline-btech/code/btmux/branches/btmux-hcode-xdr@543 e60b412a-5d77-420b-80c2-6cef64185626

--- src/db_xdr.h
+++ src/db_xdr.h
@@ -1,6 +1,8 @@
 /*
  * db_rw.h
  */
+#ifndef __DB_XDR_H__
+
 #include "copyright.h"
 
 struct mmdb_t {
@@ -11,33 +13,30 @@ struct mmdb_t {
 	int fd;
 };
 
-struct mmdb_t *mmdb_open_read(char *filename);
+struct mmdb_t *_open_read(char *filename);
+struct mmdb_t *_open_write(char *filename);
+void mmdb_resize(struct mmdb_t *, int length);
+void mmdb_close(struct mmdb_t *);
+void *_read(struct mmdb_t *, void *dest, int length);
+void mmdb_write(struct mmdb_t *, void *data, int length);
+void mmdb_write_opaque(struct mmdb_t *, void *data, int length);
 
-struct mmdb_t *mmdb_open_write(char *filename);
+void mmdb_write_uint(struct mmdb_t *, unsigned int); /* Deprecated */
+unsigned int mmdb_read_uint(struct mmdb_t *); /* Deprecated */
 
-void mmdb_resize(struct mmdb_t *mmdb, int length);
+void mmdb_write_uint8(struct mmdb_t *, uint8_t);
+uint8_t mmdb_read_uint8(struct mmdb_t *);
+void mmdb_write_uint16(struct mmdb_t *, uint16_t);
+uint16_t mmdb_read_uint16(struct mmdb_t *);
+void mmdb_write_uint32(struct mmdb_t *, uint32_t);
+uint32_t mmdb_read_uint32(struct mmdb_t *);
+void mmdb_write_uint64(struct mmdb_t *, uint64_t);
+uint64_t mmdb_read_uint64(struct mmdb_t *);
 
-void mmdb_close(struct mmdb_t *mmdb);
+void mmdb_write_single(struct mmdb_t *, float);
+float mmdb_read_single(struct mmdb_t *);
+void mmdb_write_double(struct mmdb_t *, double);
+double mmdb_read_double(struct mmdb_t *);
 
-void mmdb_write(struct mmdb_t *mmdb, void *data, int length);
 
-void mmdb_write_uint(struct mmdb_t *mmdb, unsigned int data);
-
-unsigned int mmdb_read_uint(struct mmdb_t *mmdb);
-
-void *mmdb_read(struct mmdb_t *mmdb, void *dest, int length);
-
-void mmdb_write_opaque(struct mmdb_t *mmdb, void *data, int length);
-
-void mmdb_write_object(struct mmdb_t *mmdb, dbref object);
-
-struct string_dict_entry {
-	char *key;
-	char *data;
-};
-
-static int mmdb_write_vattr(void *key, void *data, int depth, void *arg);
-
-void mmdb_db_write(char *filename);
-
-int mmdb_db_read(char *filename);
+#endif
