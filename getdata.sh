dc1de3677b5cbde41409340884e4820758ded0cb
Chris Kruger <montdidier@users.noreply.github.com>
2020-06-04 20:03:54 +0800

    fetch data from companion git repositories (#2)
    * fetch data from companion git repositories when making a build

--- getdata.sh
+++ getdata.sh
@@ -1,10 +1,9 @@
 #! /bin/sh
 
 #
-# Downloads data files to the game directory, then optionally 
-#
+# Downloads data files to the game directory, then optionally
 
-SVNROOT="svn://svn.code.sf.net/p/btonline-btech/code"
+GITORG="https://github.com/KrugerHeavyIndustries"
 
 # XXX: Terminate on errors.  This is sorta a lazy hack, to avoid having to
 # check exit status and recover on each command.
@@ -12,31 +11,35 @@ set -e
 
 # Check for prototype game directory.
 if test ! -d game; then
-	echo "Downloading game directory from the Subversion repository..."
-	svn co "${SVNROOT}/game/trunk" game
+	echo "Downloading game directory from the git repository..."
+	mkdir -p game
+	curl -L "${GITORG}/btmux-game/tarball/master" | tar xv -C game --strip-component 1
 fi
 
 # Check for prototype maps directory.
 if test ! -d game/maps; then
-	echo "Downloading game/maps from the Subversion repository..."
-	svn co "${SVNROOT}/maps/trunk" game/maps
+	echo "Downloading game/maps from the git repository..."
+	mkdir -p game/maps
+	curl -L "${GITORG}/btmux-maps/tarball/master" | tar xv -C game/maps --strip-component 1
 fi
 
 # Check for prototype text directory.
 if test ! -d game/text; then
-	echo "Downloading game/text from the Subversion repository..."
-	svn co "${SVNROOT}/text/trunk" game/text
+	echo "Downloading game/text from the git repository..."
+	mkdir -p game/text
+	curl -L "${GITORG}/btmux-text/tarball/master" | tar xv -C game/text --strip-component 1
 fi
 
 # Check for prototype mechs directory.
 if test ! -d game/mechs; then
-	echo "Downloading game/mechs from the Subversion repository..."
-	svn co "${SVNROOT}/mechs/trunk" game/mechs
+	echo "Downloading game/mechs from the git repository..."
+	mkdir -p game/mechs
+	curl -L "${GITORG}/btmux-mechs/tarball/master" | tar xv -C game/mechs --strip-component 1
 fi
 
 # Check if, for some bizarre reason, we still don't have a game directory.
 if test ! -d game; then
-	echo "No game directory. Please acquire one from http://sourceforge.net/projects/btonline-btech."
+	echo "No game directory. Please acquire one from http://github.com/KrugerHeavyIndustries/btmux."
 	exit 1
 fi
 
